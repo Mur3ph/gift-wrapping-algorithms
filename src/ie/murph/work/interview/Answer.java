49dd172de0f2bc0ebd8ca4cbff5bfe6e5f825d31
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-05 11:04:29 +0100

    workday interview algorithm

--- src/ie/murph/work/interview/Answer.java
+++ src/ie/murph/work/interview/Answer.java
@@ -3,6 +3,7 @@ package ie.murph.work.interview;
 import java.util.Scanner;
 
 import ie.murph.work.interview.emc.EMC;
+import ie.murph.work.interview.workday.Workday;
 
 public class Answer
 {
@@ -14,33 +15,10 @@ public class Answer
 	
 	System.out.println("");
 
-	System.out.println("Workday Part One");
-	int limit = 1000;
-	int total = 0;
-	for (int x = 0; x < limit; x++)
-	{
-	    if (x % 3 == 0)
-	    {
-		total = total + x;
-	    } else if (x % 5 == 0)
-	    {
-		total = total + x;
-	    }
-	}
-	System.out.println("First: " + total);
-
-	System.out.println("");
-	System.out.println("Workday Part Two");
-	total = 0;
-	for (int x = 0; x < limit; x++)
-	{
-	    if (x % 3 == 0 || x % 5 == 0)
-	    {
-		total = total + x;
-	    }
-	}
-	System.out.println("Second: " + total);
-
+	Workday workday = new Workday();
+	workday.first();
+	workday.second();
+	
 	System.out.println();
 	System.out.println("CR2.. ");
 	System.out.println("Enter number to check if it's a prime number!\n");
---
+++ src/ie/murph/work/interview/workday/Workday.java
@@ -0,0 +1,38 @@
+package ie.murph.work.interview.workday;
+
+public class Workday
+{
+    private int total = 0;
+    private int limit = 1000;
+    
+    public void first()
+    {
+	System.out.println("Workday Part One");
+	for (int x = 0; x < limit; x++)
+	{
+	    if (x % 3 == 0)
+	    {
+		total = total + x;
+	    } else if (x % 5 == 0)
+	    {
+		total = total + x;
+	    }
+	}
+	System.out.println("First: " + total);
+    }
+    
+    public void second()
+    {
+	System.out.println("");
+	System.out.println("Workday Part Two");
+	total = 0;
+	for (int x = 0; x < limit; x++)
+	{
+	    if (x % 3 == 0 || x % 5 == 0)
+	    {
+		total = total + x;
+	    }
+	}
+	System.out.println("Second: " + total);
+    }
+}
