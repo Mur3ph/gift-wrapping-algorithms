566158460369ea10ecac956eaa292f931dad66bf
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-07 16:06:52 +0100

    clean code on CR2 prime number check

--- src/ie/murph/work/interview/Answer.java
+++ src/ie/murph/work/interview/Answer.java
@@ -4,24 +4,22 @@ import ie.murph.work.interview.cr2.CR2;
 import ie.murph.work.interview.emc.EMC;
 import ie.murph.work.interview.workday.Workday;
 
-public class Answer
-{
-    public static void main(String[] args)
-    {
-	EMC emc = new EMC();
-	emc.SnapCrackleOrPop();
-	emc.reverseWord();
-	
-	System.out.println("");
+public class Answer {
+	public static void main(String[] args) {
+		EMC emc = new EMC();
+		emc.SnapCrackleOrPop();
+		emc.reverseWord();
 
-	Workday workday = new Workday();
-	workday.numberDivableByThreeAndFive();
-	workday.numberDivableByThreeAndFive2();
-	
-	System.out.println("");
-	
-	CR2 cr2 = new CR2();
-	cr2.isPrime();
-    }
+		System.out.println("");
+
+		Workday workday = new Workday();
+		workday.numberDivableByThreeAndFive();
+		workday.numberDivableByThreeAndFive2();
+
+		System.out.println("");
+
+		CR2 cr2 = new CR2();
+		cr2.isPrime();
+	}
 
 }// END OF CLASS
--- src/ie/murph/work/interview/cr2/CR2.java
+++ src/ie/murph/work/interview/cr2/CR2.java
@@ -2,25 +2,57 @@ package ie.murph.work.interview.cr2;
 
 import java.util.Scanner;
 
-public class CR2
-{
-    public void isPrime()
-    {
-	System.out.println("CR2.. ");
-	System.out.println("Enter number to check if it's a prime number!\n");
-	Scanner scanner = new Scanner(System.in);
-	int num;
-	num = scanner.nextInt();
-	boolean isItPrime = true;
-	for (int x = 2; x < num / 2; x++)
-	{
-	    if (num % x == 0)
-	    {
-		isItPrime = false;
-		break;
-	    }
+public class CR2 {
+
+	private final Scanner SCANNER = new Scanner(System.in);
+	private int userInputNumber;
+	private boolean isItPrime = true;
+	
+	public void isPrime() {
+		System.out.println("CR2.. ");
+		askUserToEnterNumber();
+		readUserInput();
+		
+		for (int x = 2; x < userInputNumber / 2; x++) {
+			if (numberIsDividableByAnotherNumber(x)) {
+				setIsItPrime(false);
+				break;
+			}
+		}
+		
+		printResult();
+		closeScanner();
 	}
-	System.out.println("Is " + num + " a prime numer: " + isItPrime);
-	scanner.close();
-    }
+	
+	private void askUserToEnterNumber() {
+		System.out.println("Enter number to check if it's a prime number!\n");
+	}
+	
+	private void readUserInput(){
+		userInputNumber = SCANNER.nextInt();
+	}
+	
+	private boolean numberIsDividableByAnotherNumber(int index){
+		return userInputNumber % index == 0;
+	}
+	
+	private void setIsItPrime(boolean isItPrime){
+		this.isItPrime = isItPrime;
+	}
+	
+	private boolean getIsItPrime(){
+		return this.isItPrime;
+	}
+	
+	private int printUserInputNumber(){
+		return userInputNumber;
+	}
+	
+	private void printResult(){
+		System.out.println("Is " + printUserInputNumber() + " a prime numer: " + getIsItPrime());
+	}
+	
+	private void closeScanner(){
+		SCANNER.close();
+	}
 }
