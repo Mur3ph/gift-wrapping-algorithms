2265479262b90b4c5246ef68821c61152082a23b
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-08 14:21:52 +0100

    Clean code, tidy workday class/methods

--- src/ie/murph/work/interview/workday/Workday.java
+++ src/ie/murph/work/interview/workday/Workday.java
@@ -1,39 +1,34 @@
 package ie.murph.work.interview.workday;
 
-public class Workday
-{
-    private int total = 0;
-    private int limit = 1000;
-    
-    public void numberDivableByThreeAndFive()
-    {
-	System.out.println("Workday Part One");
-	for (int x = 0; x < limit; x++)
-	{
-	    if (x % 3 == 0)
-	    {
-		total = total + x;
-	    } 
-	    else if (x % 5 == 0)
-	    {
-		total = total + x;
-	    }
+public class Workday {
+	private int total = 0;
+	private int limit = 1000;
+
+	public void numberDivableByThreeAndFive() {
+		System.out.println("Workday Part One");
+		for (int x = 0; x < limit; x++) {
+			if (hasNoRemainder(x, 3)) {
+				total = total + x;
+			} else if (hasNoRemainder(x, 5)) {
+				total = total + x;
+			}
+		}
+		System.out.println("First: " + total);
 	}
-	System.out.println("First: " + total);
-    }
-    
-    public void numberDivableByThreeAndFive2()
-    {
-	System.out.println("");
-	System.out.println("Workday Part Two");
-	total = 0;
-	for (int x = 0; x < limit; x++)
-	{
-	    if (x % 3 == 0 || x % 5 == 0)
-	    {
-		total = total + x;
-	    }
+
+	public void numberDivableByThreeAndFive2() {
+		System.out.println("");
+		System.out.println("Workday Part Two");
+		total = 0;
+		for (int x = 0; x < limit; x++) {
+			if (hasNoRemainder(x, 3) || hasNoRemainder(x, 5)) {
+				total = total + x;
+			}
+		}
+		System.out.println("Second: " + total);
 	}
-	System.out.println("Second: " + total);
-    }
+	
+	private boolean hasNoRemainder(int index, int numberToCheck) {
+		return index % numberToCheck == 0;
+	}
 }
