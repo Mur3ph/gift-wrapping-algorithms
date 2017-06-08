d80e1e12a12d5c7e3e5937597e55502f706eee01
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-08 09:55:13 +0100

    Clean code, tidy snap crackle and pop method

--- src/ie/murph/work/interview/emc/EMC.java
+++ src/ie/murph/work/interview/emc/EMC.java
@@ -1,39 +1,59 @@
 package ie.murph.work.interview.emc;
 
-public class EMC
-{
+public class EMC {
 
-    public void SnapCrackleOrPop()
-    {
-	System.out.println("EMC");
-	for (int x = 1; x < 51; x++)
-	{
-	    if (x % 3 == 0 && x % 5 == 0)
-	    {
-		System.out.println("Snap");
-	    } else if (x % 3 == 0)
-	    {
-		System.out.println("Crackle");
-	    } else if (x % 5 == 0)
-	    {
-		System.out.println("Pop");
-	    } else
-	    {
-		System.out.println(x);
-	    }
-	} // END OF LOOP
+	private final int MAX_NUMBER = 51;
+	private final int NUMBER_THREE = 3;
+	private final int NUMBER_FIVE = 5;
+	private int index;
 
-    }
-    
-    public void reverseWord()
-    {
-	System.out.println("");
-	String word = "Navan";
-	StringBuilder reverseIt = new StringBuilder(word);
-	reverseIt.reverse();
-	String jobsDone = reverseIt.toString();
-	System.out.println("String reverced innit: " + jobsDone);
-	System.out.println("");
-    }
-    
+	public void SnapCrackleOrPop() {
+		System.out.println("EMC");
+		for (index = 1; index < MAX_NUMBER; index++) {
+			System.out.println(snap(index));
+			System.out.println(crackle(index));
+			System.out.println(pop(index));
+			System.out.println(printIndex());
+		}
+	}
+
+	private String snap(int index) {
+		if (hasNoRemainder(index, NUMBER_THREE) && hasNoRemainder(index, NUMBER_FIVE)) {
+			return "Snap";
+		}
+		return "";
+	}
+
+	private String crackle(int index) {
+		if (hasNoRemainder(index, NUMBER_THREE)) {
+			return "Crackle";
+		}
+		return "";
+	}
+
+	private String pop(int index) {
+		if (hasNoRemainder(index, NUMBER_FIVE)) {
+			return "Pop";
+		}
+		return "";
+	}
+
+	private boolean hasNoRemainder(int index, int numberToCheck) {
+		return index % numberToCheck == 0;
+	}
+
+	private int printIndex() {
+		return this.index;
+	}
+
+	public void reverseWord() {
+		System.out.println("");
+		String word = "Navan";
+		StringBuilder reverseIt = new StringBuilder(word);
+		reverseIt.reverse();
+		String jobsDone = reverseIt.toString();
+		System.out.println("String reverced innit: " + jobsDone);
+		System.out.println("");
+	}
+
 }
