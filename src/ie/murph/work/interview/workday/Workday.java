425839c5630006f05f637f7c29a1815174ff249e
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-10 22:47:47 +0100

    vlean code, added new methods

--- src/ie/murph/work/interview/workday/Workday.java
+++ src/ie/murph/work/interview/workday/Workday.java
@@ -1,34 +1,53 @@
 package ie.murph.work.interview.workday;
 
-public class Workday {
-	private int total = 0;
-	private int limit = 1000;
+public class Workday
+{
+    private int total = 0;
+    private int limit = 1000;
 
-	public void numberDivableByThreeAndFive() {
-		System.out.println("Workday Part One");
-		for (int x = 0; x < limit; x++) {
-			if (hasNoRemainder(x, 3)) {
-				total = total + x;
-			} else if (hasNoRemainder(x, 5)) {
-				total = total + x;
-			}
-		}
-		System.out.println("First: " + total);
+    public void numberDivableByThreeAndFive()
+    {
+	System.out.println("Workday Part One");
+	for (int x = 0; x < limit; x++)
+	{
+	    numberDividableByThree(x);
+	    numberDividableByFive(x);
 	}
+	System.out.println("First: " + total);
+    }
 
-	public void numberDivableByThreeAndFive2() {
-		System.out.println("");
-		System.out.println("Workday Part Two");
-		total = 0;
-		for (int x = 0; x < limit; x++) {
-			if (hasNoRemainder(x, 3) || hasNoRemainder(x, 5)) {
-				total = total + x;
-			}
-		}
-		System.out.println("Second: " + total);
+    private void numberDividableByThree(int numberToCheckAgainst)
+    {
+	if (hasNoRemainder(numberToCheckAgainst, 3))
+	{
+	    total = total + numberToCheckAgainst;
 	}
-	
-	private boolean hasNoRemainder(int index, int numberToCheck) {
-		return index % numberToCheck == 0;
+    }
+
+    private void numberDividableByFive(int numberToCheckAgainst)
+    {
+	if (hasNoRemainder(numberToCheckAgainst, 5))
+	{
+	    total = total + numberToCheckAgainst;
 	}
+    }
+
+    public void numberDivableByThreeAndFive2()
+    {
+	System.out.println("");
+	System.out.println("Workday Part Two");
+	for (int x = 0; x < limit; x++)
+	{
+	    if (hasNoRemainder(x, 3) || hasNoRemainder(x, 5))
+	    {
+		total = total + x;
+	    }
+	}
+	System.out.println("Second: " + total);
+    }
+
+    private boolean hasNoRemainder(int index, int numberToCheck)
+    {
+	return index % numberToCheck == 0;
+    }
 }
