a23c7aaddfc72e31ebbd2abab75c23865e9c7f89
Mur3ph <paulmurphymainstreet@gmail.com>
2017-06-16 15:40:15 +0100

    clean code, refactor methods

--- src/ie/murph/work/file/comparison/read/ReadFile.java
+++ src/ie/murph/work/file/comparison/read/ReadFile.java
@@ -1,27 +1,52 @@
 package ie.murph.work.file.comparison.read;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class ReadFile {
 
 	private String filename;
+	private String currentLineOfDataFromFile;
 	
 	public ReadFile(String filename){
 		this.filename = filename;
 	}
 	
 	public void read(){
-		try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
-			String currentLine;
-			while ((currentLine = reader.readLine()) != null) {
-				System.out.println(currentLine);
+		try (BufferedReader reader = readEachLine()) {
+			while (hasData(reader)) {
+				System.out.println(printFileData());
 			}
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 	
+	private BufferedReader readEachLine(){
+		return  new BufferedReader(readFile());
+	}
+	
+	private FileReader readFile(){
+		try {
+			return new FileReader(filename);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	private boolean hasData(BufferedReader reader){
+		try {
+			return (currentLineOfDataFromFile = reader.readLine()) != null;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	private String printFileData(){
+		return currentLineOfDataFromFile;
+	}
 }
