42a92d357be9f88e281a97011a092475d45f5e0d
Mur3ph <paulmurphymainstreet@gmail.com>
2017-07-24 21:43:23 +0100

    clean code and format

--- src/ie/murph/work/interview/fibonacci/MrFibonacci.java
+++ src/ie/murph/work/interview/fibonacci/MrFibonacci.java
@@ -5,51 +5,54 @@ import java.util.Map;
 import java.util.Scanner;
 
 public class MrFibonacci {
-	
-//	Resource: https://stackoverflow.com/questions/29317414/making-fibonacci-faster?
+
+	// Resource:
+	// https://stackoverflow.com/questions/29317414/making-fibonacci-faster?
 
 	private static Map<Long, Long> previousValuesHolder;
-    static {
-        previousValuesHolder = new HashMap<Long, Long>();
-        previousValuesHolder.put(Long.valueOf(0), Long.valueOf(0));
-        previousValuesHolder.put(Long.valueOf(1), Long.valueOf(1));
-    }
-    public static long getFibonacciOf(long n) {
-        if (n== 0) {
+	static {
+		previousValuesHolder = new HashMap<Long, Long>();
+		previousValuesHolder.put(Long.valueOf(0), Long.valueOf(0));
+		previousValuesHolder.put(Long.valueOf(1), Long.valueOf(1));
+	}
 
-            return 0;
-        } else if (n == 1) {
-            return 1;
-        } else {
-            if (previousValuesHolder.containsKey(Long.valueOf(n))) {
-                return previousValuesHolder.get(n);
-            } {
+	public static long getFibonacciOf(long n) {
+		if (n == 0) {
 
-                long newValue = getFibonacciOf(n-2) + getFibonacciOf(n-1);
-                previousValuesHolder.put(Long.valueOf(n),     Long.valueOf(newValue));
-                return newValue;
-            }
+			return 0;
+		} else if (n == 1) {
+			return 1;
+		} else {
+			if (previousValuesHolder.containsKey(Long.valueOf(n))) {
+				return previousValuesHolder.get(n);
+			}
+			{
 
-        }
-    }
+				long newValue = getFibonacciOf(n - 2) + getFibonacciOf(n - 1);
+				previousValuesHolder.put(Long.valueOf(n), Long.valueOf(newValue));
+				return newValue;
+			}
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-        while (true) {
-            System.out.println("Enter n :");
-            long n = scanner.nextLong();
-            if (n >= 0) {
-                long beginTime = System.currentTimeMillis();
-                long fibo = getFibonacciOf(n);
-                long endTime = System.currentTimeMillis();
+		}
+	}
 
-                long delta = endTime - beginTime;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("Enter n :");
+			long n = scanner.nextLong();
+			if (n >= 0) {
+				long beginTime = System.currentTimeMillis();
+				long fibo = getFibonacciOf(n);
+				long endTime = System.currentTimeMillis();
 
-                System.out.println("F(" + n + ") = " + fibo + " ... computed     in " + delta + " milliseconds");
-            } else {
-                break;
+				long delta = endTime - beginTime;
 
-            }
-        }
-    }
+				System.out.println("F(" + n + ") = " + fibo + " ... computed     in " + delta + " milliseconds");
+			} else {
+				break;
+
+			}
+		}
+	}
 }
