55203a66a8a3f579d522e7aac432aec3afbfa146
Mur3ph <paulmurphymainstreet@gmail.com>
2017-07-25 21:56:47 +0100

    clean code, refactoring and tidy

--- src/ie/murph/work/interview/fibonacci/MrFibonacci.java
+++ src/ie/murph/work/interview/fibonacci/MrFibonacci.java
@@ -16,43 +16,56 @@ public class MrFibonacci {
 		previousValuesHolder.put(Long.valueOf(1), Long.valueOf(1));
 	}
 
-	public static long getFibonacciOf(long n) {
-		if (n == 0) {
-
-			return 0;
-		} else if (n == 1) {
-			return 1;
-		} else {
-			if (previousValuesHolder.containsKey(Long.valueOf(n))) {
-				return previousValuesHolder.get(n);
-			}
-			{
-
-				long newValue = getFibonacciOf(n - 2) + getFibonacciOf(n - 1);
-				previousValuesHolder.put(Long.valueOf(n), Long.valueOf(newValue));
-				return newValue;
-			}
-
-		}
-	}
-
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		while (true) {
-			System.out.println("Enter n :");
-			long n = scanner.nextLong();
-			if (n >= 0) {
+			System.out.println("Enter number: ");
+			long number = scanner().nextLong();
+			if (isNumberGreaterThanZero(number)) {
 				long beginTime = System.currentTimeMillis();
-				long fibo = getFibonacciOf(n);
+				long fibo = getFibonacciOf(number);
 				long endTime = System.currentTimeMillis();
 
 				long delta = endTime - beginTime;
 
-				System.out.println("F(" + n + ") = " + fibo + " ... computed     in " + delta + " milliseconds");
+				System.out.println("F(" + number + ") = " + fibo + " ... computed     in " + delta + " milliseconds");
 			} else {
 				break;
 
 			}
 		}
+	}
+	
+	private static Scanner scanner(){
+		return new Scanner(System.in);
+	}
+	
+	private static boolean isNumberGreaterThanZero(long number){
+		return number >= 0;
+	}
+	
+	private static long getFibonacciOf(long number) {
+		if (isZero(number)) {
+			return 0;
+		} else if (isOne(number)) {
+			return 1;
+		} else {
+			if (previousValuesHolder.containsKey(Long.valueOf(number))) {
+				return previousValuesHolder.get(number);
+			}
+			{
+				long newValue = getFibonacciOf(number - 2) + getFibonacciOf(number - 1);
+				previousValuesHolder.put(Long.valueOf(number), Long.valueOf(newValue));
+				return newValue;
+			}
+
+		}
+	}
+	
+	private static boolean isZero(long number){
+		return number == 0;
+	}
+	
+	private static boolean isOne(long number){
+		return number == 1;
 	}
 }
