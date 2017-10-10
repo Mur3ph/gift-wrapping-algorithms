aa9dea64dfbb9571d22afd8c45b40c652912b7c9
Mur3ph <paulmurphymainstreet@gmail.com>
2017-10-10 15:23:36 +0100

    clean code refactor simple algorithm hashing

--- src/ie/murph/datastructure/hashing/Hashing.java
+++ src/ie/murph/datastructure/hashing/Hashing.java
@@ -1,24 +1,14 @@
 package ie.murph.datastructure.hashing;
 
 public class Hashing
-{
-    //Better to choose a prime number
-    // 73 is the smallest prime congruent to 1 modulo 24. 73 is the 21st prime number, 37 is the 12th prime number
-    // Binary: 21 = 10101, 73 = 1001001
-    private int hash = 73;
-    
+{   
     public static void main(String[] args){
-	Hashing hashing = new Hashing();
+	SimpleHashCode simpleHash = new SimpleHashCode();
 	
-	System.out.println(hashing.hashString("Paul"));
-	System.out.println(hashing.hashString("luaP"));
-    }
-    
-    private int hashString(String userInput){
-	int lenOfInput = userInput.length();
-	for(int index = 0; index < lenOfInput; index++){
-	    hash = hash*31 + userInput.charAt(index);
-	}
-	return hash;
+	simpleHash.setHashString("Paul");
+	System.out.println(simpleHash.getHashString());
+	
+	simpleHash.setHashString("luaP");
+	System.out.println(simpleHash.getHashString());
     }
 }
