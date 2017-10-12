fbc03c626239413fd38fee8f4d163d8c03315dd5
Mur3ph <paulmurphymainstreet@gmail.com>
2017-10-12 09:57:48 +0100

    clean code, removed errors from unit test case by adding log4j jar

--- src/ie/murph/datastructure/hashing/hashmap/PaulyHashMapTest.java
+++ src/ie/murph/datastructure/hashing/hashmap/PaulyHashMapTest.java
@@ -1,46 +1,57 @@
 package ie.murph.datastructure.hashing.hashmap;
 
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PaulyHashMapTest
 {
-    public class RobsHashMapTest {
-	 
-	    private PaulyHashMap robsHashMap;
-	    private final int NUM_ELEMENTS = 200000;
-	 
-	    @Before
-	    public void setUp(){
-	        robsHashMap = new PaulyHashMap();
-	    }
-	 
-	    @Test
-	    public void testHashMapPut(){
-	        robsHashMap.put("Test1", "Test1Value");
-	    }
-	 
-	    @Test
-	    public void testPutGet(){
-	        String k = "TestPutGet";
-	        String v = "TestPutGetValue";
-	        robsHashMap.put(k, v);
-	        String valueResult = robsHashMap.get(k);
-	        assertEquals(v, valueResult);
-	    }
-	 
-	    @Test
-	    public void testHashMapCollisions() {
-	        /* Initialise hashmap */
-	        for(int i = 0; i < NUM_ELEMENTS; i++){
-	            robsHashMap.put(Integer.toString(i), Integer.toString(i));
-	        }
-	        /* Test all values of the get method */
-	        for(int i = 0; i < NUM_ELEMENTS; i++){
-	            String value = robsHashMap.get(Integer.toString(i));
-	            assertEquals(Integer.toString(i), value);
-	        }
-	    }
-	 
-	    @After
-	    public void tearDown(){
-	        robsHashMap = null;
-	    }
+    private PaulyHashMap paulsHashMap;
+    private final int NUM_ELEMENTS = 200000;
+
+    @Before
+    public void setUp()
+    {
+	paulsHashMap = new PaulyHashMap();
+    }
+
+    @Test
+    public void testHashMapPut()
+    {
+	paulsHashMap.put("Test1", "Test1Value");
+    }
+
+    @Test
+    public void testPutGet()
+    {
+	String k = "TestPutGet";
+	String v = "TestPutGetValue";
+	paulsHashMap.put(k, v);
+	String valueResult = paulsHashMap.get(k);
+	assertEquals(v, valueResult);
+    }
+
+    @Test
+    public void testHashMapCollisions()
+    {
+	/* Initialise hashmap */
+	for (int i = 0; i < NUM_ELEMENTS; i++)
+	{
+	    paulsHashMap.put(Integer.toString(i), Integer.toString(i));
+	}
+	/* Test all values of the get method */
+	for (int i = 0; i < NUM_ELEMENTS; i++)
+	{
+	    String value = paulsHashMap.get(Integer.toString(i));
+	    assertEquals(Integer.toString(i), value);
+	}
+    }
+
+    @After
+    public void tearDown()
+    {
+	paulsHashMap = null;
+    }
 }
