3b4a6cf205a5c72f02f2c6609c7a83d6e0398819
Mur3ph <paulmurphymainstreet@gmail.com>
2018-01-17 15:39:22 +0000

    added resource link to more java learning tasks

--- src/ie/murph/resources/ResourceForAlgorithmsToLearn.java
+++ src/ie/murph/resources/ResourceForAlgorithmsToLearn.java
@@ -2,10 +2,12 @@ package ie.murph.resources;
 
 public class ResourceForAlgorithmsToLearn {
 	
-	private final String GeeksforGeeksTopTenAlgorithms = "http://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/";
+	private final String GeeksforGeeksTopTenAlgorithms = "1). http://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/";
+	private final String RosettaCodeProgrammingTasks = "2). http://rosettacode.org/wiki/Category:Programming_Tasks";
 	
-	public String printTaskSheet(){
-		return GeeksforGeeksTopTenAlgorithms;
+	public final String printTaskSheet(){
+		return "\n" + GeeksforGeeksTopTenAlgorithms +
+			   "\n" + RosettaCodeProgrammingTasks;
 	}
 
 }
