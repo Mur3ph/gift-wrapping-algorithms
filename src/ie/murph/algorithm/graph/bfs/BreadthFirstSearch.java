0d77ffa010c926f595f4b263ec57e973f8ace734
Mur3ph <paulmurphymainstreet@gmail.com>
2018-01-23 15:37:46 +0000

    clean code, change variable to more appropiate

--- src/ie/murph/algorithm/graph/bfs/BreadthFirstSearch.java
+++ src/ie/murph/algorithm/graph/bfs/BreadthFirstSearch.java
@@ -8,6 +8,6 @@ public class BreadthFirstSearch {
 //	Perhaps use a map, key for nodes ,and value for visited(boolean)
 	private int startOfAlgorithm = 2; // use input from terminal
 	private int[] locations = {2, 5, 9, 6, 8, 3};
-	private Map<Integer, Boolean> travelNodes = new HashMap<Integer, Boolean>();
+	private Map<Integer, Boolean> destinationNodes = new HashMap<Integer, Boolean>();
 
 }
