2da2b6b1c2b4bdd754abde296693f03ddf60b865
Mur3ph <paulmurphymainstreet@gmail.com>
2018-10-16 22:35:33 +0100

    resources

--- src/main/java/ie/murph/resources/ResourceForAlgorithmsToLearn.java
+++ src/main/java/ie/murph/resources/ResourceForAlgorithmsToLearn.java
@@ -4,10 +4,12 @@ public class ResourceForAlgorithmsToLearn {
 	
 	private final String GeeksforGeeksTopTenAlgorithms = "1). http://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/";
 	private final String RosettaCodeProgrammingTasks = "2). http://rosettacode.org/wiki/Category:Programming_Tasks";
+	private final String GitHubAlgorithms = "3). https://github.com/TheAlgorithms/Java";
 	
 	public final String printTaskSheet(){
 		return "\n" + GeeksforGeeksTopTenAlgorithms +
-			   "\n" + RosettaCodeProgrammingTasks;
+			   "\n" + RosettaCodeProgrammingTasks +
+			   "\n" + GitHubAlgorithms;
 	}
 
 }
